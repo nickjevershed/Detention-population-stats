d67fd149bb1b5b67a1a3fcec40754d123a97e164
nickjevershed <nick.evershed@gmail.com>
2015-10-26 15:54:49 +1100

    fucking annoying PDFs

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ urlList = []
 mainPage = requests.get('https://www.border.gov.au/about/reports-publications/research-statistics/statistics/live-in-australia/immigration-detention').content
 mainPageDom = lxml.html.fromstring(mainPage)
 
-urlList2 = ['https://www.border.gov.au/ReportsandPublications/Documents/statistics/immigration-detention-statistics-feb2014.pdf']
+urlList2 = ['https://www.border.gov.au/ReportsandPublications/Documents/statistics/immigration-detention-statistics-dec2014.pdf']
 
 for url in mainPageDom.cssselect('.ym-gbox-left a'):
 	if url.attrib['href'] != "/":
@@ -48,6 +48,7 @@ def getStatsPage(pages):
 
 
 for url in urlList:
+	data = {}
 	print "getting " + url
 	pdfData = requests.get(url).content
 	xmlData = scraperwiki.pdftoxml(pdfData)
@@ -55,5 +56,51 @@ for url in urlList:
 	root = lxml.etree.XML(xmlData, parser)
 	pages = list(root)
 	statsPage = getStatsPage(pages)
-	for el in pages[statsPage]:
-		print el.attrib, gettext_with_bi_tags(el)
+
+	totalColumnPos = []
+
+	for i, el in enumerate(pages[statsPage]):
+		elText = cleanString(gettext_with_bi_tags(el))
+		# print elText
+		# print el.attrib
+		if elText == 'total':
+			leftPos = el.attrib['left']
+			totalIndex = i
+			break
+		if elText == 'women  children  total':
+			leftPos = '592'
+			totalIndex = i
+			break	
+	
+	for i, el in enumerate(pages[statsPage]):		
+		if (i > totalIndex) and (int(el.attrib['left']) < int(leftPos) + 20) and (int(el.attrib['left']) > int(leftPos) - 20):
+			# print gettext_with_bi_tags(el)	
+			totalColumnPos.append(i)
+
+	#print totalColumnPos
+	
+	# for i in totalColumnPos:
+	# 	print gettext_with_bi_tags(pages[statsPage][i])
+
+	for i, el in enumerate(pages[statsPage]):
+		elText = cleanString(gettext_with_bi_tags(el))
+		if ("christmas island idc" in elText) or ("christmas island immigration detention" in elText) or ("island immigration detention" in elText):
+			print "christmas island idc"
+			for total in totalColumnPos:
+				if total > i:
+					#print total
+					data['Christmas Island IDC'] = cleanString(gettext_with_bi_tags(pages[statsPage][total]))
+					break
+	
+	for i, el in enumerate(pages[statsPage]):
+		elText = cleanString(gettext_with_bi_tags(el))
+		if ("maribyrnong idc" in elText) or ("maribyrnong" in elText):
+			print "maribyrnong idc"
+			for total in totalColumnPos:
+				if total > i:
+					#print total
+					data['Maribyrnong IDC'] = cleanString(gettext_with_bi_tags(pages[statsPage][total]))
+					break				
+
+	print data				
+	
